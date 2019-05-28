8f1cad61bedf422ecf8690b6b4e97ba7ae3d748d
martLeveMart <mart.leveque69@gmail.com>
2019-05-28 15:04:35 +0200

    Bouton à l'ouverture améne vers écran liste d'image

--- app/src/main/java/com/example/applicationloader/MainActivity.java
+++ app/src/main/java/com/example/applicationloader/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.applicationloader;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onImg(View v){
+        Intent i = new Intent(this, liste_img.class);
+        startActivity(i);
     }
 }
